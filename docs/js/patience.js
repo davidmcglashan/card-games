0277e47702a199d232e8886ab2d0667dcbaea9c9
David McGlashan <5269729+davidmcglashan@users.noreply.github.com>
2025-07-12 10:46:34 +0100

    Patience back to about where it was. Still unfinished though ...

--- docs/js/patience.js
+++ docs/js/patience.js
@@ -1,4 +1,7 @@
 const game = {
+	/**
+	 * A new game starts with a new shuffled deck.
+	 */
 	start: () => {
 		game.deck = dealer.newShuffledCardArray()
 	},
@@ -40,39 +43,54 @@ const game = {
 	/**
 	 * Is the pile in question currently interactive?
 	 */
-	canClickOrDragFromPile: ( pile ) => {
+	canClickOrDragFromPileAtXY: ( pile, x, y ) => {		
 		// The deck is always interactive, if it has cards.
 		if ( pile.name === 'deck' ) {
-			if ( dealer.piles['deck'].cards.length + dealer.piles['drawn'].cards.length > 0 ) {
-				return true
+			if ( pile.cards.length + dealer.piles['drawn'].cards.length > 0 ) {
+				let topCard = dealer.peekTopOfPile( pile.name )
+				if ( topCard && cardUI.xyIsInBounds( x, y, topCard.elem ) ) {
+					return 2
+				}
 			} else {
-				return false
+				return 0
 			}
 		}
 
-		// Towers can be clicked if their top card is face down. 
+		// The draw pile is always interactive, if it has cards.
+		if ( pile.name === 'drawn' ) {
+			if ( pile.cards.length > 0 ) {
+				let topCard = dealer.peekTopOfPile( pile.name )
+				if ( cardUI.xyIsInBounds( x, y, topCard.elem ) ) {
+					return 2
+				}
+			} else {
+				return 0
+			}
+		}
+
+		// Towers can be clicked if their top card is face down, so you can turn it over.
 		if ( pile.name.startsWith( 'tower-' ) ) {
-			if ( pile.cards.length === 0 ) {
-				return false
+			// Disallow clicks on empty pile
+			if ( pile.cards.length === 0 && cardUI.xyIsInBounds( x, y, pile.elem ) ) {
+				return 0
 			}
 
 			let topCard = dealer.peekTopOfPile( pile.name )
-			if ( !topCard.isFaceUp ) {
-				return true
+			if ( cardUI.xyIsInBounds( x, y, topCard.elem ) ) {
+				return 2
 			}
-
-			return false
 		}
 
-		return false
+		return 0
 	},
 
 	/**
-	 * Respond to clicks on the various piles
+	 * Respond to clicks on cards  ... 
+	 *  - On the deck's top card this deals three cards.
 	 */
 	clickOnCard: ( cardName, pileName ) => {
+		// Deal three if there's cards in the deck.
 		if ( pileName === 'deck' ) {
-			// Deal three if there's cards in the deck.
 			if ( dealer.piles['deck'].cards.length > 0 ) {
 				let hand = dealer.drawFromPile( 'deck', 3 )
 				for ( let card of hand ) {
@@ -84,9 +102,23 @@ const game = {
 				return true
 			}
 		}
+
+		// Turn over face down cards at the top of towers.
+		if ( pileName.startsWith( 'tower-' ) ) {
+			let card = dealer.peekTopOfPile( pileName )
+			if ( card ) {
+				card.isFaceUp = true
+				cardUI.decorate( card )
+			}
+		}
+
 		return false
 	},
 
+	/**
+	 * Respond to clicks on piles  ... 
+	 *  - On the empty deck this resets the deck to start dealing again.
+	 */
 	clickOnPile: ( pileName ) => {
 		if ( pileName === 'deck' ) {
 			// If the deck is empty we return everything drawn back to it.
@@ -110,7 +142,6 @@ const game = {
 		return false
 	},
 
-
 	/**
 	 * Can a drag be started from the pile in question using card?
 	 */
@@ -123,12 +154,29 @@ const game = {
 		return false
 	},
 
-	canDrop: ( card, pile ) => {
+	/**
+	 * Respond to a request for dropping a card on a pile. For patience this means ...
+	 *  - you can drop onto a suit pile if it's the next card - 2 follows 1, follows A
+	 *  - you can drop on a tower when it number and value correspond: "red 7 on black 8"
+	 */
+	canDropCardAtXYOnPile: ( card, x, y, pile ) => {
 		// Permit the building of suits onto the suit piles 
 		if ( pile.name === ( 'suit-' + card.suit ) && pile.cards.length === card.ordValue ) {
-			return true
+			// Dropping an ace on an empty pile.
+			if ( pile.cards.length === 0 && cardUI.xyIsInBounds( x, y, pile.elem ) ) {
+				return 1
+			}
+
+			// Dropping the next card onto a pile with cards already.
+			if ( pile.cards.length > 0 ) {
+				let topCard = dealer.peekTopOfPile( pile.name )
+				if ( cardUI.xyIsInBounds( x, y, topCard.elem ) ) {
+					return 2
+				}
+			}
+
 		}
-		return false
+		return 0
 	},
 
 	/**
@@ -137,4 +185,11 @@ const game = {
 	dropHappened: ( card, startPileName, endPileName ) => {
 		//
 	},
+
+	/**
+	 * Returns 0 if the game isn't finished, 1 if the player loses, 2 if the player wins!
+	 */
+	hasFinished: () => {
+		return 0
+	}
 };
